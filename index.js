2bc56846091bc9a957bd2b681a81b001297f4ca0
李通洲 <carter.li@eoitek.com>
2020-02-20 09:58:39 +0800

    Print build date, regenerate js file

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsdom_1 = require("jsdom");
 void async function main() {
     const dom = await jsdom_1.JSDOM.fromURL('https://kernel.ubuntu.com/~kernel-ppa/mainline/daily/current/');
+    const { document } = dom.window;
+    console.log(document.querySelector('h1').textContent, '\n');
     const files = [];
     let a = dom.window.document.querySelector('a[href="BUILD.LOG.amd64"]');
     while (!(a = a.nextElementSibling).matches('a[href="BUILD.LOG.arm64"]')) {
-        console.log(a.textContent, a.innerHTML);
         if (/[^a-z](all|generic)[^a-z]/.test(a.textContent)) {
             files.push(a.href);
         }
--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { JSDOM } from 'jsdom';
 
 void async function main() {
   const dom = await JSDOM.fromURL('https://kernel.ubuntu.com/~kernel-ppa/mainline/daily/current/');
+  const { document } = dom.window;
+  console.log(document.querySelector('h1').textContent, '\n');
   const files: string[] = [];
   let a: HTMLAnchorElement = dom.window.document.querySelector('a[href="BUILD.LOG.amd64"]');
   while (!(a = a.nextElementSibling as HTMLAnchorElement).matches('a[href="BUILD.LOG.arm64"]')) {
