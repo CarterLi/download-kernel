880034c890a8fc35c1a0182439f6a29ca5c2db01
李通洲 <carter.li@eoitek.com>
2021-11-08 09:25:42 +0000

    Support arches other than amd64

--- index.ts
+++ index.ts
@@ -6,15 +6,25 @@ void async function main(version?: string) {
     return;
   }
 
+  const arch = {
+    arm: 'armhf',
+    arm64: 'arm64',
+    x32: 'i386',
+    x64: 'amd64',
+    ppc64: 'ppc64el',
+    s390x: 's390x',
+  }[process.arch];
+
+  if (!arch) throw new Error('Unknown arch: ' + process.arch);
+
   const dom = await JSDOM.fromURL(`https://kernel.ubuntu.com/~kernel-ppa/mainline/${/^v\d+\.\d+/.test(version) ? encodeURIComponent(version) : 'daily/current'}/`);
   const { document } = dom.window;
-  console.info(document.querySelector('h1').textContent, '\n');
-  const files: string[] = [];
-  let a: HTMLAnchorElement = dom.window.document.querySelector('a[href="amd64/log"]');
-  while (!(a = a.nextElementSibling as HTMLAnchorElement).matches('a[href="amd64/self-tests/log"]')) {
-    if (/[^a-z](all|generic)[^a-z]/.test(a.textContent)) {
-      files.push(a.href);
-    }
-  }
-  console.log(`wget ${files.join(' ')}`);
+  console.info(document.querySelector('h1').textContent, '-', arch, '\n');
+  const code: HTMLElement = document.querySelector('code');
+  const files = [
+    (code.querySelector('a[href$="_all.deb"]') as HTMLAnchorElement).href,
+    ...[...code.querySelectorAll(`a[href^="${arch}/linux-"]`)].filter((a: HTMLAnchorElement) => a.textContent.includes('-generic_')),
+  ];
+
+  console.log(`wget ${files.join(' \\\n')}`);
 }(process.argv[process.argv.length - 1]).catch(console.error);
